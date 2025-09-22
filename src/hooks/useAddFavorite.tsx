d079f50a453275ebda16bb8280f15298d904a816
fronik <askolya23@gmail.com>
2025-09-22 20:33:02 +0300

    [*] - update config

--- src/hooks/useAddFavorite.tsx
+++ src/hooks/useAddFavorite.tsx
@@ -1,13 +1,19 @@
-import { useMutation } from '@tanstack/react-query'
-import { addFavorite } from '../api/favorites'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import api from '../config/axiosConfig'
 
 export const useAddFavorite = (
   setMessage: React.Dispatch<React.SetStateAction<string>>,
 ) => {
+  const queryClient = useQueryClient()
+
   return useMutation<unknown, Error, number>({
-    mutationFn: addFavorite,
+    mutationFn: async (id: number) => {
+      const res = await api.post('/favorites', { id })
+      return res
+    },
     onSuccess: (res: any) => {
       setMessage(res.message)
+      queryClient.invalidateQueries({ queryKey: ['get activities'] })
     },
     onError: error => {
       setMessage(error.message)
